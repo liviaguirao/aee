c41ad5385d2d47d9992b463a8140bbc29266e6d4
Leony <leonyhelenos@gmail.com>
2024-09-28 14:13:31 -0300

    Arrumando pastas

--- front-end/src/pages/App.jsx
+++ front-end/src/pages/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./App.css"; // Certifique-se de que esse arquivo tem os estilos corretos
-import Agenda from "./components/Agenda"
+import "../styles/App.css"; // Certifique-se de que esse arquivo tem os estilos corretos
+import Agenda from "../components/Agenda"
 
 // Função principal do App
 function App() {
